802755072fb10136a26b5321e131a37b2bc02e12
ramza007 <ramza007@gmail.com>
2018-04-21 22:21:38 +0300

    added config module

--- config.py
+++ config.py
@@ -1,11 +1,31 @@
+import os
+
+
 class Config:
-    pass
+    """Main configurations class"""
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ramza:ramza123@localhost/blog'
+
+    SECRET_KEY = "ramza123"
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+
+
 
 class ProdConfig(Config):
-    pass
+    """Production configuration class that inherits from the main configurations class"""
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ramza:ramza123@localhost/blog'
+
 
 class DevConfig(Config):
+    """Configuration class for development stage of the app"""
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig}
 
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
