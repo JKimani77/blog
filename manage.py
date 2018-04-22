9f6484cfbcba407cd601754dd280af51a669bca1
ramza007 <ramza007@gmail.com>
2018-04-22 10:07:01 +0300

    added manage test and shell

--- manage.py
+++ manage.py
@@ -12,6 +12,20 @@ manager.add_command('server', Server)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def test():
+    """Run unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comments=Comments, PitchCategory=PitchCategory)
+    pass
+
 if __name__ == '__main__':
     manager.run()
 
