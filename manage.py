c9131c65e9e30ebae42f09482fe2cd5f38278e2e
ramza007 <ramza007@gmail.com>
2018-04-22 10:06:28 +0300

    added to manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
-
 from flask_script import Manager,Server
 from app import create_app,db
 
-app = create_app('default')
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
-
 manager.add_command('server', Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
